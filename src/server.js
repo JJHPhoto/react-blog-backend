f1113177f826dd2d5205a1e3a3af205a2a993289
Joshua J. Haller <phone@joshuajhaller.com>
2021-07-15 12:59:59 -0700

    Refactored code related to database by creating a DRY function. Used function in GET request for articles by name. Tested on Postman. Working.

--- src/server.js
+++ src/server.js
@@ -6,24 +6,30 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get("/api/articles/:name", async (req, res) => {
+const withDB = async (operations) => {
   try {
-    const articleName = req.params.name;
-
     const client = await MongoClient.connect("mongodb://localhost:27017", {
       useNewUrlParser: true,
     });
     const db = client.db("react-blog");
 
-    const articleInfo = await db
-      .collection("articles")
-      .findOne({ name: articleName });
-    res.status(200).json(articleInfo);
+    await operations(db);
 
     client.close();
   } catch (error) {
     res.status(500).json({ message: "Error connecting to db", error });
   }
+};
+
+app.get("/api/articles/:name", async (req, res) => {
+  withDB(async (db) => {
+    const articleName = req.params.name;
+
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(articleInfo);
+  });
 });
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
