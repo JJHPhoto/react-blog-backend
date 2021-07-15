77faf981d7b5875aade9d4c64c965a92ae958661
Joshua J. Haller <phone@joshuajhaller.com>
2021-07-15 13:14:13 -0700

    Used refactored function in Upvote and AddComment POST routes. Tested with Postman. Found error due to missing one await command. Working now.

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(bodyParser.json());
 
-const withDB = async (operations) => {
+const withDB = async (operations, res) => {
   try {
     const client = await MongoClient.connect("mongodb://localhost:27017", {
       useNewUrlParser: true,
@@ -29,17 +29,12 @@ app.get("/api/articles/:name", async (req, res) => {
       .collection("articles")
       .findOne({ name: articleName });
     res.status(200).json(articleInfo);
-  });
+  }, res);
 });
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
-  try {
+  withDB(async (db) => {
     const articleName = req.params.name;
-
-    const client = await MongoClient.connect("mongodb://localhost:27017", {
-      useNewUrlParser: true,
-    });
-    const db = client.db("react-blog");
 
     const articleInfo = await db
       .collection("articles")
@@ -57,20 +52,30 @@ app.post("/api/articles/:name/upvote", async (req, res) => {
       .findOne({ name: articleName });
 
     res.status(200).json(updatedArticleInfo);
-
-    client.close();
-  } catch (error) {
-    res.status(500).json({ message: "Error connecting to db", error });
-  }
+  }, res);
 });
 
 app.post("/api/articles/:name/add-comment", (req, res) => {
   const { username, text } = req.body;
   const articleName = req.params.name;
 
-  articlesInfo[articleName].comments.push({ username, text });
-
-  res.status(200).send(articlesInfo[articleName]);
+  withDB(async (db) => {
+    const articleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    await db.collection("articles").updateOne(
+      { name: articleName },
+      {
+        $set: {
+          comments: articleInfo.comments.concat({ username, text }),
+        },
+      }
+    );
+    const updatedArticleInfo = await db
+      .collection("articles")
+      .findOne({ name: articleName });
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 });
 
 app.listen(8000, () => console.log("listening on port 8000"));
